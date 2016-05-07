d4db1a8053af3a170e412469282874608d7b0a5d
Eric Lin <ericlin@Erics-MacBook-Pro-2.local>
2016-05-07 00:34:30 -0700

    fix

--- deploy/ensure-k8s-installed.sh
+++ deploy/ensure-k8s-installed.sh
@@ -17,7 +17,7 @@ export K8S_VERSION=1.2.2
 
 cd ~/kubernetes
 
-wget "http://storage.googleapis.com/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64/kubectl"
+wget "https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl"
 chmod 755 kubectl
 PATH=$PATH:`pwd`
 
